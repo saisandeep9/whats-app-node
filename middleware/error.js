2da1c020d6cb2d6ca5bf54d1f2d8004524866f87
saisandeep <43948146+saisandeep9@users.noreply.github.com>
2020-08-24 11:03:35 +0530

    first commit

--- middleware/error.js
+++ middleware/error.js
@@ -2,8 +2,8 @@ const winston = require("winston");
 
 function error(err, req, res, next) {
   // winston.error(err.message, err);
+  res.status(404).send({ error: "Invalid Url" });
   res.status(500).send({ error: "An error occurred", err });
-  // res.status(404).send({ error: "Invalid category is provided" });
 }
 
 module.exports = error;
